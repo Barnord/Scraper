1f7d90519be28ccb3256110f4849d97f8ade7d48
Ben Arno <bdarno92@gmail.com>
2022-11-22 11:44:35 -0800

    initial commit

---
+++ scraper/scraper/Core/ScraperHtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scraper.Core
+{
+    public class ScraperHtml
+    {
+        static HttpClient _client = new HttpClient();
+
+        public static async Task<string> HttpRequestAsync(string address)
+        {
+            if (Uri.IsWellFormedUriString(address, UriKind.Absolute)) {
+                _client.BaseAddress = new Uri(address);
+            }
+            //else
+            //{
+            //    Uri.
+            //}
+            //_client.G
+
+            return $"HTTPREQUEST RETURN: {await _client.GetAsync(_client.BaseAddress)}";
+        }
+    }
+}
---
+++ scraper/scraper/Program.cs
@@ -0,0 +1,26 @@
+﻿using scraper.Core;
+
+namespace scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enter();
+        }
+        static async Task Enter()
+        {
+            Console.WriteLine("Please enter an address");
+            string ?address = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(await ScraperHtml.HttpRequestAsync(address));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
